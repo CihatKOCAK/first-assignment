5d2b1274d72070b2228c5668b1755c0f704edbbe
Cihat KOÇAK <pcihatkocakks@gmail.com>
2021-09-24 23:41:42 +0300

    final touches v1
    final touches v1

--- search.js
+++ search.js
@@ -11,6 +11,7 @@ function search() {
         name = response.data.name;
         username = response.data.login;
         pict = response.data.avatar_url;
+        console.log(response.data);
 
 
         reposLink = response.data.repos_url;
@@ -55,6 +56,10 @@ function search() {
             for (let i = 0; i < lang.length; i++)
                 document.getElementById("addToMe").innerHTML += '<div class="child flex-child"><i class="cbp-ig-icon devicon-' + lang[i].toLowerCase() + '-plain"></i><p">' + lang[i] + '</p><p">' + Math.round((percent[i] * 100) / allCodeSize) + '%</p></div>';
             document.getElementById("repos").innerText = repos.length + "repo(s)";
+
+            if (allCodeSize < 1000)
+            document.getElementById("codeSize").innerText = allCodeSize  + "KB code";
+            else
             document.getElementById("codeSize").innerText = allCodeSize / 1000 + "MB code";
             document.getElementById("name").innerText = "Name: " + name;
             document.getElementById("username").innerText = "User Name: " + username;
